fd1cf3dc158ab58c1384afaa4c75bc80d81f9524
Kyle Pollina <kylepollina@gmail.com>
2018-12-21 15:26:49 -0600

    Updated authentication to JWT

--- src/settings.py
+++ src/settings.py
@@ -164,15 +164,16 @@ STATIC_ROOT = 'static'
 
 # Settings for Django Rest Framework
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
+
 
 # Does not require trailing forward slash on url POSTs
 APPEND_SLASH = False
