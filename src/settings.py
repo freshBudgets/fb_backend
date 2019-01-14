a084ffad0c232dcefb5f389871c98f1356ad8f44
Kyle Pollina <kylepollina@gmail.com>
2019-01-14 09:07:26 -0500

    Travis

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ import os
 
 if os.environ.get('PRODUCTION'):
     from .production import *
-elif os.environ.get('STAGING'):
-    from .production import *
+# elif os.environ.get('STAGING'):
+#     from .production import *
 else:
     from .development import * 
